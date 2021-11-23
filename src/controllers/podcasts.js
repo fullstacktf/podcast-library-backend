641b205b1ec5e51e8046d98a6fb7da03f2fdbc80
Rafa Gandolfo Garcia <rafagandolfogarcia@gmail.com>
2021-11-23 16:58:02 +0100

    endpoint

--- src/controllers/podcasts.js
+++ src/controllers/podcasts.js
@@ -54,6 +54,7 @@ router.get("/genre/:genre", (req, res) => {
   res.send({ podcast });
 });
 
+
 router.get("/autor/:autor", (req, res) => {
   const autor = req.params.autor.toLowerCase();
   const podcast = podcasts.filter(
@@ -61,5 +62,96 @@ router.get("/autor/:autor", (req, res) => {
   );
   res.send({ podcast });
 });
+
+router.post("/podcast",(req,res)=>{
+
+  if(bodyisEmpty(req.body)){
+      res.status(400).send('No disponible');
+  }else{
+      podcasts.push({id: req.params.id, title: req.params.title, author: req.params.author, episode: req.params.episode, description: req.params.description, image: req.params.image, language: req.params.language, url: req.params.url, genre: req.params.genre });
+      res.send({podcasts})
+  }
+
+});
+router.post("/usuarios",(req,res)=>{
+
+  if(bodyisEmpty(req.body)){
+      res.status(400).send('No disponible');
+  }else{
+      podcasts.push({id: req.params.id,nombre: req.params.nombre, email: req.params.email, contraseña: req.params.contraseña});
+      res.send({podcasts})
+  }
+
+});
+
+
+router.delete('/usuario/:id', (req,res)=>{
+  if(bodyisEmpty(req.body)){
+      res.status(400).send("No se ha encontrado el usuario a eliminar");
+  }else{
+      const usuarioId = req.params.id();
+      const usuarioEncontrado = usuarios.findIndex((usuario) =>usuario.id ===usuarioId)
+
+      if(podcast.index ===-1){
+          res.status(400).send("No se ha encontrado el usuario a eliminar");
+      }else{
+          usuarios.splice(usuarioEncontrado,1)
+          res.send({usuarios})
+      }
+    }
+});
+
+/* router.delete('/usuario/:id', (req,res)=>{
+  if(bodyisEmpty(req.body)){
+      res.status(400).send("No se ha encontrado el usuario a eliminar");
+  }else{
+      const usuarioId = req.params.id();
+      const usuarioEncontrado = usuarios.findIndex((usuario) =>usuario.id ===usuarioId)
+
+      if(usuarioEncontrado ===-1){
+          res.status(400).send("No se ha encontrado el usuario a eliminar");
+      }else{
+          usuarios = usuarios.filter((usuario) =>usuario.id !==usuarioId)
+          res.send({usuarios})
+      }
+  }
+}); */
+
+
+
+/* router.delete('/podcast/:title', (req,res)=>{
+  if(bodyisEmpty(req.body)){
+      res.status(400).send("No se ha encontrado el titulo a eliminar");
+  }else{
+      const title = req.params.title.LowerCase();
+      const podcastEncontrado = podcasts.findIndex((podcast) =>podcast.title.LowerCase() ===title)
+
+      if(podcastEncontrado ===-1){
+          res.status(400).send("No se ha encontrado el titulo a eliminar");
+      }else{
+          podcasts = podcasts.filter((podcast) =>podcast.title.LowerCase() !== title)
+          res.send({podcasts})
+      }
+  }
+});
+ */
+
+router.delete('/podcast/:title', (req,res)=>{
+  if(bodyisEmpty(req.body)){
+      res.status(400).send("No se ha encontrado el titulo a eliminar");
+  }else{
+      const title = req.params.title.LowerCase();
+      const podcastEncontrado = podcasts.findIndex((podcast) =>podcast.title.LowerCase() ===title)
+
+      if(podcastEncontrado===-1){
+          res.status(400).send("No se ha encontrado el titulo a eliminar");
+      }else{
+          podcasts.splice(podcastEncontrado,1)
+          res.send({podcasts})
+      }
+  }
+})
+
+
 
 module.exports = router;
