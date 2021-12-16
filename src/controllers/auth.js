a2a926bcc218f02a6970307c96a56495192134fb
Pablo Hdez <pheralb@DESKTOP-6BNJL06.localdomain>
2021-12-16 22:17:03 +0000

    🔨 Fix jwt.sign

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -54,6 +54,7 @@ router.post("/login", async (req, res) => {
 
     const token = jwt.sign({
         username: user.username,
+        email: user.email,
         id: user._id
     }, process.env.TOKEN_SECRET);
 
