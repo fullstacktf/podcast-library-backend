77380c85b2fc551991dfd8907c0c9fbef098d60f
Rafa Gandolfo Garcia <rafagandolfogarcia@gmail.com>
2021-12-17 21:20:09 +0100

    test de users funcionan 3, y get user by id?

--- tests/user.test.js
+++ tests/user.test.js
@@ -16,40 +16,51 @@ var agent = chai.request.agent(url)
 describe('Authenticate a user: ',()=>{
     it('should receive an OK and a cookie with the authentication token', (done) => {
     agent
-    .get('/authentication')
-      .auth('user', 'password')
-    .end( function(err,res){
-    console.log(res.body)
-    expect(res).to.have.status(200);
-    return agent.get('/user')
-          .then(function (res) {
-            expect(res).to.have.status(200);
-            console.log(res.body)
-            done();
-          });
-    done();
-    });
-    });
-   });
-   
-
-   describe('delete the user with id 1: ',()=>{
-    it('should delete the user with id 1', (done) => {
-    chai.request(url)
-    .get('/user/1')
-    .end( function(err,res){
-    console.log(res.body)
-    expect(res.body).to.have.lengthOf(2);
-    expect(res).to.have.status(200);
-    chai.request(url)
-    .del('/user/1')
+    .post('/user/login')
+    .send({password:'richito',email:'emaillll@emaaaaail.com'})
     .end( function(err,res){
     console.log(res.body)
     expect(res).to.have.status(200);
     done();
-    });
     });
     });
 });
    
-     
+describe('Insert a user with a form: ',()=>{
+    it('should insert an user ', (done) => {
+    chai.request(url)
+    .post('/user/register')
+    .send({username:"rafaaaaaaaaaa",password:"rafaaaaaaaaaa",email:"rafa@emaaail.com"})
+    .end( function(err,res){
+    console.log(res.body)
+    expect(res).to.have.status(200);
+    done();
+    });
+    });
+});
+   
+
+describe('delete the user with id 1: ',()=>{
+    it('should delete the user with id 1', (done) => {
+    chai.request(url)
+    .del('/user/61bb7849cf85101ac16a8a18')
+    .end( function(err,res){
+    console.log(res.body)
+    expect(res).to.have.status(200);
+    done();
+    });
+    });
+});
+
+
+describe('get the user with id 61bce768656b45e15d623f19: ',()=>{
+    it('should get the user with id 61bce768656b45e15d623f19', (done) => {
+    chai.request(url)
+    .get('/user/61bce768656b45e15d623f19')
+    .end( function(err,res){
+    console.log(res.body)
+    expect(res).to.have.status(200);
+    done();
+    });
+    });
+});    
